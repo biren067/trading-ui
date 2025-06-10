246afd75f4e2699573356f988ef5ca3af6a15ade
Biren <birendrabitian@gmail.com>
2025-06-10 08:47:53 -0700

    refactor11

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-   images: {
-    domains: ["trading-backend-zcks.onrender.com"],
-  },
+  //  images: {
+  //   domains: ["trading-backend-zcks.onrender.com"],
+  // },
 };
 
 export default nextConfig;
